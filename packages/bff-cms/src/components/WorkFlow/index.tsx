52e4decf63d5f34ad51e0759783478423b1295dd
maqun <markexin@gmail.com>
2022-01-18 19:43:17 +0800

    feat: 完成选择弹窗逻辑

--- packages/bff-cms/src/components/WorkFlow/index.tsx
+++ packages/bff-cms/src/components/WorkFlow/index.tsx
@@ -1,114 +1,104 @@
-import React, {
-  useEffect,
-  useRef,
-  useState,
-  useMemo,
-  FC,
-  ReactNode,
-} from 'react';
-import { Graph } from '@antv/x6';
+import React, { useEffect, useRef, useState, FC } from 'react';
 import { Col, Row, Modal, Card } from '@douyinfe/semi-ui';
+import { Graph } from '@antv/x6';
 import { IconWifi, IconCode } from '@douyinfe/semi-icons';
 import useStateCallback from '@/hooks/useStateCallback';
 import { PostTools } from '../PostTools';
 import { CodeEditor } from '../CodeEditor';
 import { config, GraphHoc, portSync, nodeSync, plusSync } from './chartTools';
-import { useCallback } from 'react';
 
-// TODO: 此处处理的不好
-// eslint-disable-next-line no-unused-vars
-const TypeCard: FC<{
-  onChange: Function;
-}> = (props) => {
+// 选择类型弹窗
+const ModelContent: FC<{
+  visible: boolean;
+  onChange?: Function;
+  onCancel?: Function;
+}> = ({ onChange, onCancel, visible }) => {
+  const [nodeType, setNodeType] = useState('');
+
   const { Meta } = Card;
-  const [nodeType, setNodeType] = useState<string>('');
-  const template = useMemo(
-    () => [
-      {
-        _key: 'fetch',
-        style: {
-          background:
-            nodeType === 'fetch' ? 'var(--semi-color-primary)' : '#fff',
-          color: nodeType === 'fetch' ? '#fff' : '#000',
-        },
-        cover: <IconWifi size='large' style={{ marginTop: '20px' }} />,
-        meta: {
-          title: (
-            <span style={{ color: nodeType === 'fetch' ? '#fff' : '#000' }}>
-              API接口
-            </span>
-          ),
-        },
+  const template = [
+    {
+      _key: 'fetch',
+      style: {
+        background: nodeType === 'fetch' ? 'var(--semi-color-primary)' : '#fff',
+        color: nodeType === 'fetch' ? '#fff' : '#000',
       },
-      {
-        _key: 'handler',
-        style: {
-          background:
-            nodeType === 'handler' ? 'var(--semi-color-primary)' : '#fff',
-          color: nodeType === 'handler' ? '#fff' : '#000',
-        },
-        cover: <IconCode size='large' style={{ marginTop: '20px' }} />,
-        meta: {
-          title: (
-            <span style={{ color: nodeType === 'handler' ? '#fff' : '#000' }}>
-              函数处理
-            </span>
-          ),
-        },
+      cover: <IconWifi size='large' style={{ marginTop: '20px' }} />,
+      meta: {
+        title: (
+          <span style={{ color: nodeType === 'fetch' ? '#fff' : '#000' }}>
+            API接口
+          </span>
+        ),
       },
-    ],
-    [nodeType],
-  );
-
-  useEffect(() => {
-    return () => {
-      // setNodeType('');
-    };
-  }, []);
+    },
+    {
+      _key: 'handler',
+      style: {
+        background:
+          nodeType === 'handler' ? 'var(--semi-color-primary)' : '#fff',
+        color: nodeType === 'handler' ? '#fff' : '#000',
+      },
+      cover: <IconCode size='large' style={{ marginTop: '20px' }} />,
+      meta: {
+        title: (
+          <span style={{ color: nodeType === 'handler' ? '#fff' : '#000' }}>
+            函数处理
+          </span>
+        ),
+      },
+    },
+  ];
 
   return (
-    // @ts-ignore
-    <Row type='flex' justify='space-around' align='middle'>
-      {template.map((node, index) => (
-        <Col span={12} key={index}>
-          <Card
-            style={node.style}
-            cover={node.cover}
-            // @ts-ignore
-            onClick={() => {
-              props.onChange(template[index]._key);
-              setNodeType(template[index]._key);
-            }}>
-            <Meta
-              style={{ textAlign: 'center', display: 'block' }}
-              title={node.meta.title}
-            />
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <Modal
+      title='请选择模块类型'
+      visible={visible}
+      onOk={() => {
+        onChange && onChange(nodeType);
+        setNodeType('');
+      }}
+      onCancel={() => onCancel && onCancel()}>
+      <Row type='flex' justify='space-around' align='middle'>
+        {template.map((node, index) => (
+          <Col span={12} key={index}>
+            <Card
+              style={node.style}
+              cover={node.cover}
+              // @ts-ignore
+              onClick={() => {
+                setNodeType(template[index]._key);
+              }}>
+              <Meta
+                style={{ textAlign: 'center', display: 'block' }}
+                title={node.meta.title}
+              />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Modal>
   );
 };
 
 export const WorkFlow = () => {
   const editRef = useRef<HTMLDivElement>(null);
   const [nodes, setNode] = useStateCallback([]);
-  const [currentType, setCurrentType] = useState<'handler' | 'fetch' | ''>('');
+  const [visible, setVisible] = useState<boolean>(false);
   const [viewControl, setViewControl] = useState<'handler' | 'fetch' | ''>('');
+  const graph = useRef();
 
   // 增加节点
-  const handleNodePlus = (_context: Graph, _id: string) => {
-    console.log(currentType);
-  };
-
-  console.log(currentType, '======');
+  function handleNodePlus(nodeType: string) {
+    console.log(nodeType, graph.current, '============');
+  }
 
   // 初始化编辑器注入
   useEffect(() => {
     const width = editRef.current?.clientWidth!;
     const height = document.body.clientHeight - 90;
 
-    const graph = new GraphHoc({
+    (graph.current as unknown as Graph) = new GraphHoc({
       container: editRef.current!,
       width,
       height,
@@ -116,24 +106,13 @@ export const WorkFlow = () => {
     });
 
     // 处理事件
-    graph?.on('cell:click', ({ cell }) => {
+    (graph.current as unknown as Graph).on('cell:click', ({ cell }) => {
       const { _key } = cell.data;
       if (_key === 'handler' || _key === 'fetch') {
         setViewControl(_key);
       } else {
-        Modal.info({
-          title: '请选择模块类型',
-          // @ts-ignore
-          content: (
-            <TypeCard
-              onChange={(v) => {
-                console.log(v, '------');
-                setCurrentType(v);
-              }}
-            />
-          ),
-          onOk: () => handleNodePlus(graph, cell.id),
-        });
+        //
+        setVisible(!visible);
       }
     });
 
@@ -146,20 +125,30 @@ export const WorkFlow = () => {
         nodeSync('4', '结束', width / 2, 210),
         portSync('5', '2', '4'),
       ],
-      (n: any) => graph?.fromJSON(n),
+      (n: any) => (graph.current as unknown as Graph).fromJSON(n),
     );
   }, []);
 
   return (
-    <Row gutter={30}>
-      <Col span={12}>
-        <div ref={editRef}></div>
-      </Col>
-      <Col span={12}>
-        {viewControl === 'handler' ?? <CodeEditor />}
-        {viewControl === 'fetch' ?? <PostTools />}
-        <div>welcome</div>
-      </Col>
-    </Row>
+    <>
+      <Row gutter={30}>
+        <Col span={12}>
+          <div ref={editRef}></div>
+        </Col>
+        <Col span={12}>
+          {viewControl === 'handler' ?? <CodeEditor />}
+          {viewControl === 'fetch' ?? <PostTools />}
+          <div>welcome</div>
+        </Col>
+      </Row>
+      <ModelContent
+        visible={visible}
+        onChange={(t: string) => {
+          handleNodePlus(t);
+          setVisible(!visible);
+        }}
+        onCancel={() => setVisible(!visible)}
+      />
+    </>
   );
 };
